c2b3a95175a3113283142d849462084f758a1249
Awais Ansari <ansariawais161@outlook.com>
2024-07-26 18:39:57 +0530

    Add project files.

---
+++ JwtProjectEx/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using JwtProjectEx.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtProjectEx.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ JwtProjectEx/ServiceContainer.cs
@@ -0,0 +1,17 @@
+﻿using JwtProjectEx.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtProjectEx
+{
+    public static class ServiceContainer
+    {
+        public static void RegisterDependencies(IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(opt =>
+            {
+                string? connectionString = configuration["ConnectionStrings:DbConnection"];
+                opt.UseSqlServer(connectionString);
+            });
+        }
+    }
+}

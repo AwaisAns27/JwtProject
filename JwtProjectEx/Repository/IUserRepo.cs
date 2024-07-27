053e2f6423fbf6243d2f0efc16a2a6cdc246132e
Awais Ansari <ansariawais161@outlook.com>
2024-07-27 15:01:40 +0530

    No changes

---
+++ JwtProjectEx/Repository/IUserRepo.cs
@@ -0,0 +1,12 @@
+﻿using JwtProjectEx.ApiResponses;
+using JwtProjectEx.Dtos;
+
+namespace JwtProjectEx.Repository
+{
+    public interface IUserRepo
+    {
+        public Task<RegisterUserResponse> RegisterUserAsync(RegisterUserDto registerUserDto);
+        public Task<LogInUserResponse> LogInUserAsync(LogInUserDto logInUserDto);
+
+    }
+}

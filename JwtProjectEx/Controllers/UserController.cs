a8fc10be7dcce7ea1edcfa243f87d812c433b0bf
Awais Ansari <ansariawais161@outlook.com>
2024-07-27 15:00:57 +0530

    Code in Controller

---
+++ JwtProjectEx/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using JwtProjectEx.ApiResponses;
+using JwtProjectEx.Dtos;
+using JwtProjectEx.Entities;
+using JwtProjectEx.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtProjectEx.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepo _userRepo;
+
+        public UserController(IUserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+        public async Task<ActionResult<ApplicationUser>> Register(RegisterUserDto registerUserDto)
+        {
+            var result = _userRepo.RegisterUserAsync(registerUserDto);
+            return Ok(result);
+
+        }
+
+        public async Task<ActionResult<LogInUserResponse>> LogIn(LogInUserDto logInUserDto)
+        {
+           var result = _userRepo.LogInUserAsync(logInUserDto);
+            return Ok(result);
+        }
+    }
+}
